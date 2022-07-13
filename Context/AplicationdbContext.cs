42d40b67c185866b78ccb0f43ad451d81cae3e8a
arianatzucv@gmail.com <arianatzucv@gmail.com>
2022-07-12 19:18:23 -0500

    Agregar archivos de proyecto.

---
+++ Context/AplicationdbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoU123BV.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoU123BV.Context
+{
+    public class AplicationdbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server = localhost; database = proyectou1; user = root; password = ");
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Modelo/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoU123BV.Modelo
+{
+    public class Usuario
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+    }
+}
